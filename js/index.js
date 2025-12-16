966515a4350d530aabe3637f0afcfd81fb925027
Seif-96 <seif365.s.seif@gmail.com>
2025-12-16 22:40:39 +0200

    add js

--- js/index.js
+++ js/index.js
@@ -54,8 +54,89 @@ settingsToggle.addEventListener("click", function () {
   settingsSidebar.classList.remove("translate-x-full");
   // settingsToggle.classList.add("translate-x-[-100%]");
 });
+
 // close settingsToggle
 let closeSettings = document.querySelector("#close-settings");
+// closeSettings
 closeSettings.addEventListener("click", function () {
   settingsSidebar.classList.add("translate-x-full");
 });
+// fonts
+let alexandriaFont = document.querySelector("#alexandriaFont");
+let tajawalFont = document.querySelector("#tajawalFont");
+let cairoFont = document.querySelector("#cairoFont");
+// tajawalFont
+tajawalFont.addEventListener("click", function () {
+  document.body.classList.add("font-tajawal");
+  document.body.classList.remove("font-alexandria");
+  document.body.classList.remove("font-cairo");
+  tajawalFont.classList.add("active");
+  alexandriaFont.classList.remove("active");
+  cairoFont.classList.remove("active");
+});
+// alexandriaFont
+alexandriaFont.addEventListener("click", function () {
+  document.body.classList.add("font-alexandria");
+  document.body.classList.remove("font-tajawal");
+  document.body.classList.remove("font-cairo");
+  alexandriaFont.classList.add("active");
+  tajawalFont.classList.remove("active");
+  cairoFont.classList.remove("active");
+});
+// cairoFont
+cairoFont.addEventListener("click", function () {
+  document.body.classList.add("font-cairo");
+  document.body.classList.remove("font-tajawal");
+  document.body.classList.remove("font-alexandria");
+  cairoFont.classList.add("active");
+  alexandriaFont.classList.remove("active");
+  tajawalFont.classList.remove("active");
+});
+// form
+// customSelectWrapper
+let customSelectWrapper = document.querySelector("#customSelectWrapper");
+// chevronDown
+let chevronDown = document.querySelector("#chevronDownOne");
+// listbox
+let listbox = document.querySelector('[role="listbox"]');
+// open listbox
+customSelectWrapper.addEventListener("click", function () {
+  listbox.classList.toggle("hidden");
+  chevronDown.classList.toggle("rotate-180");
+});
+// close listbox
+document.addEventListener("click", function (e) {
+  if (!customSelectWrapper.contains(e.target)) {
+    listbox.classList.add("hidden");
+    chevronDown.classList.remove("rotate-180");
+  }
+});
+// options
+let selectedText = customSelectWrapper.querySelector(".selected-text");
+let options = listbox.querySelectorAll(".custom-option");
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    let value = this.dataset.value;
+    selectedText.textContent = value;
+    listbox.classList.add("hidden");
+    chevronDown.classList.remove("rotate-180");
+  });
+});
+// // customSelectWrapperTwo
+// let customSelectWrapperTwo = document.querySelector("#customSelectWrapperTwo");
+// // chevronDown
+// let chevronDownTwo = document.querySelector("#chevronDownTwo");
+// // listboxTwo
+// let listboxTwo = document.querySelector("#listboxTwo");
+// // open listboxTwo
+// customSelectWrapperTwo.addEventListener("click", function () {
+//   listboxTwo.classList.toggle("hidden");
+//   chevronDownTwo.classList.toggle("rotate-180");
+// });
+// // close listboxTwo
+// document.addEventListener("click", function (e) {
+//   if (!customSelectWrapperTwo.contains(e.target)) {
+//     listboxTwo.classList.toggle("hidden");
+//     chevronDownTwo.classList.remove("rotate-180");
+//   }
+// });
